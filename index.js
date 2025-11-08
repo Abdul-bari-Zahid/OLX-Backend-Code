25e5896e58d6e76df400b206ce7f8168bbde86f2
Abdul-bari-Zahid <bariblogger6@gmail.com>
2025-11-08 11:09:26 +0500

    check

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: ["https://olx-frontend-code-78om.vercel.app", "http://olx-admin-code-ri2j.vercel.app/"],
+  origin: [
+    "https://olx-frontend-code-78om.vercel.app",
+    "https://olx-admin-code-ri2j.vercel.app"
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
